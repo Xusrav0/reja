c6d02abb3af143ce9cc4acd509a087b089a749de
Xusrav0 <xusravshohidjonov@gmail.com>
2026-08-27 19:27:15 +0900

    feat: completed portfolio publishing

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 kirish code
 app.use(express.static("public")); // browserdan kelgan zaproslar uchun (clientlarga ochib berish uchun)
@@ -28,6 +38,10 @@ app.post("/create-item", (req, res) => {
   // malumotni uzi bilan olib keladi
   console.log(req);
   res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
