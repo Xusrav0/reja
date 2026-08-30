ec9bf0403bc08edbe80c7f83bcdb0bcddefe6a22
Xusrav0 <xusravshohidjonov@gmail.com>
2026-08-30 13:47:01 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,55 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// MongoDB calling
+const db = require("./server").db("Reja");
+
+// 1 kirish code
+// app.use(express.static("public")); Middleware Design Pattern
+app.use(express.static("public")); // browserdan kelgan zaproslar uchun (clientlarga ochib berish uchun)
+app.use(express.json()); // kirib kelayotgan json ko'rinishidagi datani bizga objectga o'girib beradi
+app.use(express(express.urlencoded({ extended: true }))); // html dan traditional form request dan post qilingan narsalarni qabul qilishga ishlatilinadi
+
+// 2 Session code
+
+// 3 Views code
+// BSSR
+app.set("views", "views");
+app.set("view engine", "ejs"); // ejs orqali frontendni yasimiz backendni ichida
+
+// 4 Routing code
+
+// app.get("/hello", function (req, res) {
+//   // res.end(`<h1> Hello World </h1>`);
+//   res.end(`<h1 style="background: red"> Hello World by AUSTIN</h1>`); // localhost:3000/hello
+// });
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1> Siz sovg'alar bo'limidasiz </h1>`); // localhost:3000/gift
+// });
+app.post("/create-item", (req, res) => {
+  // malumotni uzi bilan olib keladi
+  console.log(req);
+  res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  // data base dan malumotni olish uchun get ishlatilinadi
+  res.render("reja");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,59 +1,29 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+let db;
+const connectionString =
+  "mongodb+srv://stevenfurry041_db_user:QLHbDOhSXR82YcmF@cluster0.mrmrbno.mongodb.net/";
 
-// 1 kirish code
-// app.use(express.static("public")); Middleware Design Pattern
-app.use(express.static("public")); // browserdan kelgan zaproslar uchun (clientlarga ochib berish uchun)
-app.use(express.json()); // kirib kelayotgan json ko'rinishidagi datani bizga objectga o'girib beradi
-app.use(express(express.urlencoded({ extended: true }))); // html dan traditional form request dan post qilingan narsalarni qabul qilishga ishlatilinadi
-
-// 2 Session code
-
-// 3 Views code
-// BSSR
-app.set("views", "views");
-app.set("view engine", "ejs"); // ejs orqali frontendni yasimiz backendni ichida
-
-// 4 Routing code
-
-// app.get("/hello", function (req, res) {
-//   // res.end(`<h1> Hello World </h1>`);
-//   res.end(`<h1 style="background: red"> Hello World by AUSTIN</h1>`); // localhost:3000/hello
-// });
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1> Siz sovg'alar bo'limidasiz </h1>`); // localhost:3000/gift
-// });
-app.post("/create-item", (req, res) => {
-  // malumotni uzi bilan olib keladi
-  console.log(req);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  // data base dan malumotni olish uchun get ishlatilinadi
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${3000}`,
-  );
-});
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection:", err);
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${3000}`,
+        );
+      });
+    }
+  },
+);
